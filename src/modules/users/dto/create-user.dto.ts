9f12ee78f1693f5a8e126f4851b07abe83e4c3ab
lucas-lima-dev <lucas.sdslima@gmail.com>
2023-07-02 18:12:36 -0300

    feat setting validations with class-validator

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,10 +1,31 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from "class-validator";
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  private _name: string;
+  @IsNotEmpty()
+  @IsEmail()
+  @IsString()
+  private _email: string;
+  @IsNotEmpty()
+  @IsString()
+  @Length(6, 20)
+  private _password: string;
+  @IsNotEmpty()
+  @IsString()
+  private _avatar: string;
+
   constructor(
-    private _name: string,
-    private _email: string,
-    private _password: string,
-    private _avatar: string
-  ) {}
+    nameParams: string,
+    emailParams: string,
+    passwordParams: string,
+    avatarParams: string
+  ) {
+    this._name = nameParams;
+    this._email = emailParams;
+    this._password = passwordParams;
+    this._avatar = avatarParams;
+  }
 
   get name(): string {
     return this._name;
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UsePipes,
+  ValidationPipe,
+} from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
 
-@Controller('users')
+@Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @UsePipes(ValidationPipe)
   create(@Body() createUserDto: CreateUserDto): void {
-     this.usersService.create(createUserDto);
+    this.usersService.create(createUserDto);
   }
 
   @Get()
@@ -17,18 +28,18 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.usersService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.usersService.remove(+id);
   }
 }
